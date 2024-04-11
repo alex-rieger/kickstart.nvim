7fd3d7963dd8dd6cefc40c3fd6481e7f34b03786
Alex <alex@pop-os.fritz.box>
2024-04-11 18:42:37 +0200

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
 return {}
