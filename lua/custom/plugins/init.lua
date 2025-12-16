1c97766cb85094fbbd3ebc49077f1104b1bd03a4
Alex Rieger <alex@rieger.sh>
2025-12-16 16:49:30 +0100

    add go config

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,9 @@
 -- See the kickstart.nvim README for more information
 
 require 'custom.plugins.rose-pine'
+require 'custom.plugins.go'
+require 'custom.plugins.dap'
+require 'custom.plugins.dap-go'
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -13,5 +16,15 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
 vim.g.have_nerd_font = true
+
+-- go format on save
+local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*.go',
+  callback = function()
+    require('go.format').gofmt()
+  end,
+  group = format_sync_grp,
+})
 
 return {}
